736865399cf9c739b1aa1e67e312b17fd10fe316
mkrishna09 <m.krishna090405@gmail.com>
2026-07-12 03:38:16 +0530

    Logging Middleware updated for detailed logs

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,16 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import logger from "../logger/logger";
 
-
-
 export function requestLogger(
   req: Request,
   res: Response,
   next: NextFunction
 ): void {
-  logger.info(
-  `${req.method} ${req.originalUrl}`
-);
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+
+    logger.info({
+      method: req.method,
+      url: req.originalUrl,
+      statusCode: res.statusCode,
+      responseTime: `${duration}ms`,
+      ip: req.ip,
+    });
+  });
 
   next();
 }
